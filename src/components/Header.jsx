fce729318d64caeabb213a4ce1d007d115190df3
Morning <morning@admins-iMac.local>
2023-01-16 12:33:36 +0800

    2nd

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,19 @@
 import React from "react";
 import "../style/Header.css";
 export default function Header() {
-  return <header>Header</header>;
+  return (
+    <header className="d-flex">
+      <div className="image">
+        <img src={"Group_5.png"} alt="png" />
+      </div>
+      <div className="input-btn">
+        <input type="text" placeholder="Search any things" />
+        <button>Search</button>
+      </div>
+      <div className="bag">
+        <img src={"Group 3.png"} alt="" />
+        <img src={"Frame 7.svg"} alt="" />
+      </div>
+    </header>
+  );
 }
